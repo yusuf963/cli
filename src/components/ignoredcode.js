d08387af5d27945dbbd69cd1d45a410c4ce3967a
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-29 09:42:01 +0100

    add readme instuctiona and update font color

---
+++ src/components/ignoredcode.js
@@ -0,0 +1,48 @@
+// const cryptoData = require('../data.json');
+// Fetching mock data
+
+    // .then(response => setData(response.data))
+    // .catch(e => console.log(e))
+    // setData(cryptoData);
+
+// <Box borderStyle='single' padding={2} flexDirection='column'>
+//   <Box>
+//     <Box width='25%'><Text>COIN</Text></Box>
+//     <Box width='25%'><Text>PRICE (USD)</Text></Box>
+//     <Box width='25%'><Text>24 HOUR CHANGE</Text></Box>
+//     <Box width='25%'><Text>ALL TIME HIGH</Text></Box>
+//   </Box>
+//   <Newline />
+//   {
+//     data.map(({ id, name, current_price, price_change_percentage_24h, ath }) => (
+//       <Box key={id}>
+//         <Box width='25%'>
+//           <Text>{name}</Text>
+//         </Box>
+//         <Box width='25%'>
+//           <Text color='cyan'>{'$' + current_price.toLocaleString()}</Text>
+//         </Box>
+//         <Box width='25%'>
+//           <Text backgroundColor={Math.sign(price_change_percentage_24h) < 0 ? 'red' : 'green'}>
+//             {price_change_percentage_24h.toFixed(2) + '%'}
+//           </Text>
+//         </Box>
+//         <Box width='25%'>
+//           <Text color='green'>{'$' + ath.toLocaleString()}</Text>
+//         </Box>
+//       </Box>
+//     ))
+//   }
+
+
+{/* {
+        data.map(({ id, name, current_price, price_change_percentage_24h, ath }) => (
+          <Box key={id}>
+            <Box width='25%'><Text>{name}</Text></Box>
+            <Box width='25%'><Text>{current_price}</Text></Box>
+            <Box width='25%'><Text>{price_change_percentage_24h}</Text></Box>
+            <Box width='25%'><Text>{ath}</Text></Box>
+          </Box>
+        ))
+      } */}
+{/* </Box> */ }
--- ui.js
+++ ui.js
@@ -5,11 +5,13 @@ const BigText = require('ink-big-text');
 const { Text } = require('ink');
 const importJsx = require('import-jsx');
 const Table = importJsx('./src/components/Table')
-
 const App = ({ name = 'any' }) => (
   <>
     <Gradient name="summer" >
       <BigText text="crypto cli" align='center' font='chrome' />
+    </Gradient>
+    <Gradient name="summer" >
+      <Text align='center' font='chrome'>Powered By M.Yusuf</Text>
     </Gradient>
     <Table />
   </>
